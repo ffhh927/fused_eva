42f893fcaef886caa0093e51a129b066daae5d1d
ffhh927 <102224883+ffhh927@users.noreply.github.com>
2024-06-03 14:40:51 +0800

    cutlass-python-6.3-v2

--- fused/fused_eva/fused_eva.py
+++ fused/fused_eva/fused_eva.py
@@ -1,7 +1,7 @@
 import math
 import torch
 import cutlass
-# import time
+import time
 
 import torch.optim as optim
 import numpy as np
@@ -13,7 +13,7 @@ import logging
 logger = logging.getLogger()
 from fused.multi_tensor_apply import multi_tensor_applier
 #import amp_C
-
+import grouped_gemm
 class Fused_KFAC(optim.Optimizer):
     """Accelerate Distributed K-FAC with Sublinear Memory Cost
     Args:
@@ -68,17 +68,16 @@ class Fused_KFAC(optim.Optimizer):
 
         self.steps = 0
 
-        # if multi_tensor_applier.available:
-        #     import amp_C
-        #     # Skip buffer
-        #     self._dummy_overflow_buf = torch.tensor([0], dtype=torch.int, device='cuda')
-        #     self.multi_tensor_eva = amp_C.multi_tensor_eva
-        #     self.multi_group = amp_C.multi_group
-        # else:
+       # if multi_tensor_applier.available:
+      #      import amp_C
+            # Skip buffer
+       #     self._dummy_overflow_buf = torch.tensor([0], dtype=torch.int, device='cuda')
+       #     self.multi_tensor_eva = amp_C.multi_tensor_eva
+            #self.multi_group = amp_C.multi_group
+        #else:
         #    raise RuntimeError('apex.optimizers.FusedEva requires cuda extensions')
+        #####cutlass
         
-        # cutlass
-        self.plan = cutlass.op.GroupedGemm(element=torch.float32, layout=cutlass.LayoutType.RowMajor)
         
 
     ### Register hooks
@@ -97,8 +96,8 @@ class Fused_KFAC(optim.Optimizer):
                     self.m_a[module].mul_(1-self.factor_decay).add_(new, alpha=self.factor_decay)
                     #xi =  math.pow(self.steps+1, -self.factor_decay)
                     #self.m_a[module].mul_(1-xi).add_(new, alpha=xi)
-            if backend.comm.size() > 1:
-                self.handles.append(backend.comm.allreduce_async_(self.m_a[module], op=backend.comm.Average))
+            # if backend.comm.size() > 1:
+            #     self.handles.append(backend.comm.allreduce_async_(self.m_a[module], op=backend.comm.Average))
 
     def _backward_hook_event(self, module, grad_input, grad_output):
         """Default: hook for saving gradient w.r.t output (g)"""
@@ -112,8 +111,8 @@ class Fused_KFAC(optim.Optimizer):
                     self.m_g[module].mul_(1-self.factor_decay).add_(new, alpha=self.factor_decay)
                     #xi =  math.pow(self.steps+1, -self.factor_decay)
                     #self.m_g[module].mul_(1-xi).add_(new, alpha=xi)
-            if backend.comm.size() > 1:
-                self.handles.append(backend.comm.allreduce_async_(self.m_g[module], op=backend.comm.Average))
+            # if backend.comm.size() > 1:
+            #     self.handles.append(backend.comm.allreduce_async_(self.m_g[module], op=backend.comm.Average))
 
     def _register_module_hooks(self, model):
         """Register forard/backward hooks to supported modules"""
@@ -131,15 +130,15 @@ class Fused_KFAC(optim.Optimizer):
                 module_name = 'module_name_%s_%d' % (classname, name_idx)
                 self.module_names.append(module_name)
                 name_idx += 1
-        if backend.comm.rank() == 0:
-#         if backend._HorovodBackend().rank() == 0:
+#         if backend.comm.rank() == 0:
+# #         if backend._HorovodBackend().rank() == 0:
             
-            logger.info("#register modules: %s", len(self.modules))
+#             logger.info("#register modules: %s", len(self.modules))
 
 	### Precondition gradients
     def _precondition_grads(self):
         """Compute preconditioned gradients via Eva"""
-     
+
         g_sum = 0
         v_sum = 0
         vg_sum = 0
@@ -152,56 +151,41 @@ class Fused_KFAC(optim.Optimizer):
         ma_list_T = []
         mg_list_T = []
 
-        a=[]
-        g=[]
-        gp = []
-        ag=[]
-        ga=[]
-         
-
-        max_layer = 0
         for module in self.modules:
             # get ma, mg, grad
             ma = self.m_a[module].view(-1, 1)
             mg = self.m_g[module].view(-1, 1)
             grad = self._get_grad(module)
-
+           # print(ma.shape)
             ma_list.append(ma)
             mg_list.append(mg)
             ma_list_T.append(ma.T)
             mg_list_T.append(mg.T)
             p_grad.append(grad)
-            a.append(torch.zeros(1,1,dtype=torch.float32, device='cuda'))
-            g.append(torch.zeros(1,1,dtype=torch.float32, device='cuda'))
-            gp.append(torch.zeros(1,ma.size(0),dtype=torch.float32, device='cuda'))
-            ag.append(torch.zeros(1,1,dtype=torch.float32, device='cuda'))
-            ga.append(torch.zeros(mg.size(0),ma.size(0),dtype=torch.float32, device='cuda'))
-
 
         A = ma_list_T + mg_list_T + mg_list_T
         B = ma_list + mg_list + p_grad
-        D = a + g + gp
-        self.plan.run(A, B, D, D, print_module=False)
-        a = D[:len(a)]
-        g = D[len(a):len(a)+len(g)]
-        gp = D[len(a)+len(g):]
+        D = grouped_gemm.run(A, B)
+
+        a = D[:len(self.modules)]
+        g = D[len(self.modules):2*len(self.modules)]
+        gp = D[2*len(self.modules):]
 
         A = gp+mg_list
         B = ma_list + ma_list_T
-        D = ag +ga
-        self.plan.run(A, B, D, D, print_module=False)
-        ag = D[:len(ag)]
-        ga = D[len(ag):]
-
+        D = grouped_gemm.run(A, B)
+       
+        ag = D[:len(self.modules)]
+        ga = D[len(self.modules):]
 
         for module ,i in zip(self.modules,range(len(self.modules))):
+
             grad = self._get_grad(module)
             v1 = ga[i].mul(-ag[i]/(a[i] * g[i] + self.damping))
             v1.add_(grad)
             v1.div_(self.damping)
             v.append(v1)
 
-            
         for module ,i in zip(self.modules, range(len(self.modules))):
             if module.bias is not None:
                 weight = v[i][:, :-1].view(module.weight.grad.data.size())
@@ -268,10 +252,10 @@ class Fused_KFAC(optim.Optimizer):
         self.fac_update_freq = group['fac_update_freq']
         self.kfac_update_freq = group['kfac_update_freq']
 
-        if self.steps % self.fac_update_freq == 0 and backend.comm.size() > 1:
-            for handle in self.handles:
-                backend.comm.synchronize(handle)
-            self.handles = []
+        # if self.steps % self.fac_update_freq == 0 and backend.comm.size() > 1:
+        #     for handle in self.handles:
+        #         backend.comm.synchronize(handle)
+        #     self.handles = []
         
         self._precondition_grads()
 
